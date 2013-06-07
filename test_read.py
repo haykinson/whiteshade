c78bbf05f78fc216f6d6c376fe9efd3683773bde
Ilya Haykinson <haykinson@gmail.com>
2013-06-07 07:33:37 +0000

    actually control lights now

--- test_read.py
+++ test_read.py
@@ -1,49 +1,99 @@
-from asyncore import file_dispatcher, loop
 from evdev import InputDevice, categorize, ecodes, list_devices, events
+from phue import Bridge
+import requests
+import threading
+import time
+from select import select
 
 devices = [InputDevice(x) for x in list_devices()]
 dev = [d for d in devices if d.name == 'Mouse Pad']
 
-class InputDeviceDispatcher(file_dispatcher):
-  def __init__(self, device):
+class InputDeviceDispatcher(object):
+  def __init__(self, device, sender):
     self.device = device
+    self.sender = sender
     self.tx = 200
     self.ty = 160
-    file_dispatcher.__init__(self, device)
+
+  #def read_loop(self):
+  #  while True:
+  #    r,w,x = select([self.device.fd], [], []) #, 1) #1 sec timeout
+  #    for event in self.device.read():
+  #      yield event
+
+  def start(self):
+    for event in self.device.read_loop():
+      self.handle_event(event)
   
-  def recv(self, ign=None):
-    return self.device.read()
+  def handle_event(self, event):
+    event = categorize(event)
+    if isinstance(event, events.KeyEvent):
+      if event.event.code == ecodes.BTN_MOUSE:
+        #turn on, and to normal brightness
+        self.tx = 200
+        self.ty = 160
+        self.sender.set({'on':True,'bri':128,'hue':32500,'transitiontime':20})
+      elif event.event.code == ecodes.BTN_RIGHT:
+        self.tx = 200
+        self.ty = 160
+        self.sender.set({'on':False, 'transitiontime': 20})
+    elif isinstance(event, events.RelEvent):
+      if event.event.code == ecodes.REL_X:
+        if event.event.value < 0:
+          self.tx = max(0, self.tx + event.event.value)
+        elif event.event.value > 0:
+          self.tx = min(400, self.tx + event.event.value)
+          
+        print 'X: %d\t\t\t\tPos: (%d, %d)' % (event.event.value, self.tx, self.ty)
+      elif event.event.code == ecodes.REL_Y:
+        if event.event.value > 0:
+          self.ty = max(0, self.ty - event.event.value)
+        elif event.event.value < 0:
+          self.ty = min(320, self.ty - event.event.value)
 
-  def handle_read(self):
-    for event in self.recv():
-      event = categorize(event)
-      if isinstance(event, events.RelEvent):
-        if event.event.code == ecodes.REL_X:
-          if event.event.value < 0:
-            self.tx = max(0, self.tx + event.event.value)
-          elif event.event.value > 0:
-            self.tx = min(400, self.tx + event.event.value)
+        print '\t\tY: %d\t\tPos: (%d, %d)' % (event.event.value, self.tx, self.ty)
+      else:
+        print 'other relative event'
 
-          print 'X: %d\t\t\t\tPos: (%d, %d)' % (event.event.value, self.tx, self.ty)
-        elif event.event.code == ecodes.REL_Y:
-          if event.event.value > 0:
-            self.ty = max(0, self.ty - event.event.value)
-          elif event.event.value < 0:
-            self.ty = min(320, self.ty - event.event.value)
+      hue = int(65000.0 * (float(self.tx) / 400.0))
+      bri = int(255.0 * (float(self.ty) / 320.0))
+      command = {'bri': bri, 'hue': hue}
+      self.sender.set(command)
 
-          print '\t\tY: %d\t\tPos: (%d, %d)' % (event.event.value, self.tx, self.ty)
-        else:
-          print 'other relative event'
-      #if event.type == ecodes.REL_Y:
-      #  print "Y: %d" % event.value
-      #elif event.type == ecodes.REL_Y:
-      #  print "\t\tX: %d" % event.value
-      #print(categorize(event))
+class Sender(threading.Thread):
+  def __init__(self, bridge):
+    threading.Thread.__init__(self)
+    self.daemon = True
+    self.bridge = bridge
+    self.next = {'hue':32500,'bri': 128}
+    self.dirty = True
+
+  def run(self):
+    while (True):
+      if self.dirty:
+        self.dirty = False
+        self.bridge.set_light([1,2,3], self.next)
+        #self.bridge.set_group(1, self.next)
+      time.sleep(0.1)
+  
+  def set(self, next):
+    self.next = next
+    self.dirty = True
 
 print dev
 
 if dev and len(dev) > 0:
+  r = requests.get('http://www.meethue.com/api/nupnp')
+  d = r.json()
+  ip = d[0]['internalipaddress']
+
+  b = Bridge(ip)  
+
   dev = dev[0]
-	
-  InputDeviceDispatcher(dev)
-  loop()
+
+  sender = Sender(b)
+  sender.start()
+
+  d = InputDeviceDispatcher(dev, sender)
+  d.start()
+
